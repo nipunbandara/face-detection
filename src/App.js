f251b0686f6d66bacb62722e6bd593074a3d0e29
nipunbandara <nipun.bandara@outlook.com>
2023-04-08 11:44:40 +0530

    detect function

--- src/App.js
+++ src/App.js
@@ -10,6 +10,48 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  //load facemesh
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: {width: 640, height: 480},
+      scale: 0.8
+    });
+    setInterval(() => {
+      detect(net)
+    }, 100);
+  };
+
+  //detect function
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" && 
+      webcamRef.current !== null && 
+      webcamRef.current.video.readyState === 4) {
+        //get video properties
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+
+        //set video width
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
+
+        //set canvas width
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+
+        //make detections
+        const face = await net.estimateFaces(video);
+        console.log(face);
+
+        //get canvas context for drawing
+        //const ctx = canvasRef.current.getContext("2d");
+        //requestAnimationFrame(()=>{detect(net)});
+
+      }
+  };
+
+  runFacemesh();
   return (
     <div className="App">
       <Webcam ref={webcamRef} style={
